bc9aa83bb2787cc588dd317dc18478878f3134f7
Gregory McIntyre <greg@gregorymcintyre.com>
2015-12-10 12:22:42 +1100

    Basic React example

---
+++ reactdemo/scripts/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let Hello = React.createClass({
+  render() {
+    return(
+        <div>
+            Hello world
+        </div>
+    );
+  }
+});
+
+export default Hello;
