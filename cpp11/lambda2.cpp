92316b5959ce3745fe79baff822ec81bcad79e91
Gregory McIntyre <greg@gregorymcintyre.com>
2017-03-17 09:55:33 +1100

    C++11 learning

---
+++ cpp11/lambda2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  char s[] = "Hello World!";
+  int uppercase = 0; // modified by the lambda
+  for_each(s, s+sizeof(s), [&uppercase] (char c) {
+      if (isupper(c))
+        uppercase++;
+    });
+  cout << uppercase << " uppercase letters in: " << s <<endl;
+}
