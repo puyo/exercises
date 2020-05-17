47ca859ebb9825a47a07b06992f0f8e9f1f3afc0
Gregory McIntyre <greg@gregorymcintyre.com>
2020-05-17 15:03:45 +1000

    More comments on the tutorial

--- r/snails/snails.R
+++ r/snails/snails.R
@@ -1,23 +1,35 @@
-Snail_feeding <- read.csv("Snail_feeding.csv", header = T, strip.white = T, na.strings = "")
+# Tutorial:
+# http://environmentalcomputing.net/importing-data/
+
+Snail_feeding <- read.csv(
+  "Snail_feeding.csv",
+  header = T,
+  strip.white = T,
+  na.strings = "",
+  # NOTE: stringsAsFactors = FALSE is the default in R 4+ but not in R version 3.
+  # We pass it here so that this code works on R version 3 or 4.
+  stringsAsFactors = FALSE
+)
 
 # Only keep columns 1:7
 Snail_feeding <- Snail_feeding[,1:7]
 
-# Selects rows that are not entirely NA
+# Selects rows that are not entirely NA. Not necessary for this data set
+
 # Snail_feeding <- Snail_feeding[, colSums(is.na(Snail_feeding)) != nrow(Snail_feeding)]
-# Select columns that are not entirely NA
-# Snail_feeding <- Snail_feeding[rowSums(is.na(Snail_feeding)) != ncol(Snail_feeding), ]  
 
-# install (if not already installed) and load dplyr package
-if (!require(dplyr)) { install.packages('dplyr') }
+# Select columns that are not entirely NA. Not necessary for this data set
 
-# Rename Snail.ID to Snail using dplyr
-Snail_feeding <-
-  Snail_feeding %>%
-  rename("Snail" = "Snail.ID")
+# Snail_feeding <- Snail_feeding[rowSums(is.na(Snail_feeding)) != ncol(Snail_feeding), ]
 
 # Rename Snail.ID using built in R
-# names(Snail_feeding$Snail.ID) <- "Snail"
+names(Snail_feeding$Snail.ID) <- "Snail"
+
+# Rename Snail.ID to Snail using the dplyr package. Bit nicer if you already
+# have and use dplyr install (if not already installed) and load dplyr package
+
+# if (!require(dplyr)) { install.packages('dplyr') }
+# Snail_feeding <- rename(Snail_feeding, "Snail" = "Snail.ID")
 
 Snail_feeding$Snail <- as.factor(Snail_feeding$Snail)
 
