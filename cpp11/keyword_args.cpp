e1ca6e94fff4151cc0686ec9061dba5a1ab22a39
Gregory McIntyre <greg@gregorymcintyre.com>
2020-09-10 09:40:50 +1000

    More C99 keyword args

--- cpp11/keyword_args.cpp
+++ cpp11/keyword_args.cpp
@@ -6,12 +6,15 @@ using namespace std;
 struct mul_args{ int x; int y; };
 
 int mul(const mul_args& args) {
-  return args.x * args.y;
+  return (args.x*2) * args.y;
 }
 
 int main() {
-  cout << mul({.x = 2, .y = 3}) << endl;
-  cout << mul({.y = 2, .x = 3}) << endl;
+  cout << mul({.x = 5, .y = 4}) << endl;
+  cout << mul({.y = 4, .x = 5}) << endl;
+
+  mul_args a = {.y = 2, .x = 1};
+  cout << "x = " << a.x << ", y = " << a.y << endl;
 
   return 0;
 }
