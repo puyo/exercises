b34827f0a925fc3b8a58843b5eb6551a22f4016a
Gregory McIntyre <greg@gregorymcintyre.com>
2020-09-17 11:39:35 +1000

    C99 keyword args, does it work with refs

--- cpp11/keyword_args.cpp
+++ cpp11/keyword_args.cpp
@@ -3,9 +3,15 @@
 
 using namespace std;
 
-struct mul_args{ int x; int y; };
+struct mul_args { int x; int y; };
 
 int mul(const mul_args& args) {
+  return (args.x*2) * args.y;
+}
+
+struct mul_ref_args { const int& x; const int& y; };
+
+int mul_ref(const mul_ref_args& args) {
   return (args.x*2) * args.y;
 }
 
@@ -15,6 +21,11 @@ int main() {
 
   mul_args a = {.y = 2, .x = 1};
   cout << "x = " << a.x << ", y = " << a.y << endl;
+  cout << mul(a) << endl;
+
+  int x = 1;
+  int y = 2;
+  cout << mul_ref({.x = x, .y = y}) << endl;
 
   return 0;
 }
