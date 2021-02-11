0416954f5ad2801cf23890a10cb07bf147110160
Gregory McIntyre <greg@gregorymcintyre.com>
2021-02-11 15:05:19 +1100

    Simplify use of classlist

--- js/tampermonkey/trackkarma.js
+++ js/tampermonkey/trackkarma.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @license      Creative Commons BY-NC-SA
 // @encoding     utf-8
-// @version      1.1
+// @version      1.2
 // @description  Make it easier to mark attendance with Track Karma
 // @author       puyo
 // @include      https://app.trackkarma.com/trainings*
@@ -35,6 +35,59 @@ GM_addStyle(`
     const csrfValue = () => document.querySelector('meta[name=csrf-token]').getAttribute('content')
     const csrfParam = () => document.querySelector('meta[name=csrf-param]').getAttribute('content')
 
+    const setValue = (value, present) => {
+        const body = new URLSearchParams([
+            ['utf8', '✓'],
+            ['_method', 'patch'],
+            [csrfParam(), csrfValue()],
+            ['availability[status]', value],
+        ])
+
+        fetch(action, {
+            method: 'POST',
+            body: body,
+        })
+            .then((res) => {
+                if (res.ok) {
+                    success(present)
+                } else {
+                    console.error(res)
+                }
+            })
+            .catch((err) => console.error(err))
+    }
+
+    const success = (present) => {
+        const oldPresent = card.querySelector('.availability-present') != null
+
+        if (oldPresent === present) {
+            return
+        }
+
+        avail.classList.toggle('availability-present', present)
+        avail.classList.toggle('availability-absent', !present)
+
+        const icon = avail.querySelector('.fa')
+        icon.classList.toggle('fa-check', present)
+        icon.classList.toggle('fa-close', !present)
+
+        const attendances = card.querySelector('.attendances')
+        if (attendances) {
+            attendances.textContent = parseInt(attendances.textContent) + (present ? 1 : -1)
+        }
+    }
+
+    const makeButton = (label, klass, value, present) => {
+        const button = document.createElement('button')
+        button.classList.add('button', 'small', 'secondary', klass)
+        button.innerHTML = label
+        button.addEventListener('click', (event) => {
+            event.preventDefault()
+            setValue(value, present)
+        })
+        return button
+    }
+
     document.querySelectorAll('.training-card, .availability-card').forEach(function (card) {
         const link = card.querySelector('a[data-remote=true]')
 
@@ -47,62 +100,6 @@ GM_addStyle(`
         const avail = card.querySelector('.training-availability, .member-availability')
 
         avail.style.position = 'relative'
-
-        const setValue = (value, present) => {
-            const body = new URLSearchParams([
-                ['utf8', '✓'],
-                ['_method', 'patch'],
-                [csrfParam(), csrfValue()],
-                ['availability[status]', value],
-            ])
-
-            fetch(action, {
-                method: 'POST',
-                body: body,
-            })
-                .then((res) => {
-                    if (res.ok) {
-                        success(present)
-                    } else {
-                        console.error(res)
-                    }
-                })
-                .catch((err) => console.error(err))
-        }
-
-        const success = (present) => {
-            const oldPresent = card.querySelector('.availability-present') != null
-
-            if (oldPresent === present) {
-                return
-            }
-
-            avail.classList.toggle('availability-present', present)
-            avail.classList.toggle('availability-absent', !present)
-
-            const icon = avail.querySelector('.fa')
-            icon.classList.toggle('fa-check', present)
-            icon.classList.toggle('fa-close', !present)
-
-            const attendances = card.querySelector('.attendances')
-            if (attendances) {
-                attendances.textContent = parseInt(attendances.textContent) + (present ? 1 : -1)
-            }
-        }
-
-        const makeButton = (label, klass, value, present) => {
-            const button = document.createElement('button')
-            button.classList.add('button')
-            button.classList.add('small')
-            button.classList.add('secondary')
-            button.classList.add(klass)
-            button.innerHTML = label
-            button.addEventListener('click', (event) => {
-                event.preventDefault()
-                setValue(value, present)
-            })
-            return button
-        }
 
         const setPresent = makeButton('<i class="fa fa-check"></i>', 'set-present', 'present', true)
         const setAbsent = makeButton('<i class="fa fa-close"></i>', 'set-absent', 'absent', false)
