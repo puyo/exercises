004a7c3ae4b4d0d467d18a4951dfafe686cba6b3
Gregory McIntyre <greg@gregorymcintyre.com>
2021-06-24 06:56:53 +1000

    Add Eulalie's python

---
+++ python/laylie.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+name = input('What is your name? ')
+
+if name.lower() == 'gregory':
+    print('Greg is a poo.')
+
+elif name.lower() == 'greg':
+    print('Greg is the pooiest poo.')
+
+elif name.lower() == 'eulalie':
+    print('You are the best, Eulalie!')
+
+elif name.lower() == 'ruby':
+    print('You are always right about everything no sarcasm.')
+
+else:
+    print("I don't know you.")
+
