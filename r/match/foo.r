012ce3bf379cfc4c60b1e460ed8d404408485349
Gregory McIntyre <greg@gregorymcintyre.com>
2023-04-13 08:05:39 +1000

    R styler auto formatting

--- r/match/foo.r
+++ r/match/foo.r
@@ -12,7 +12,9 @@ extract_buds <- function(input) {
   # get rid of all contents of brackets ( ... ) e.g. (rarely 3)
   input <- gsub("\\(.*?\\)", "", input)
 
-  is_match <- function(m) { return(!is.na(m[[1]])) }
+  is_match <- function(m) {
+    return(!is.na(m[[1]]))
+  }
   reg_match <- stringr::str_match
 
   if (is_match(m <- reg_match(input, "buds? solitary"))) {
@@ -54,7 +56,7 @@ extract_buds <- function(input) {
     m1 <- NA
   }
 
-  return(list(buds_desc=desc, buds_input_type=m_type, buds_min=m0, buds_max=m1))
+  return(list(buds_desc = desc, buds_input_type = m_type, buds_min = m0, buds_max = m1))
 }
 
 add_buds_column <- function(df) {
@@ -72,33 +74,33 @@ add_buds_column <- function(df) {
 
 test_extract_buds <- function() {
   tests <- list(
-    list(min=7,  max=7,  input="Inflorescence blah, buds 7 per umbel, blah. "),
-    list(min=7,  max=13, input="Inflorescence blah, buds 7, 9, 11 or 13 per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, 7 buds per umbel, rarely 3, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds 7(–11) per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds per umbel 7, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds usually 7 (rarely 3) per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds more than 7 per umbel, blah. "),
-    list(min=7,  max=9,  input="Inflorescence blah, buds 7 or more commonly 9 per umbel, blah. "),
-    list(min=9,  max=9,  input="Inflorescence blah, buds 9 (rarely 11) per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds per umbel 7, blah. "),
-    list(min=9,  max=15, input="Inflorescence blah, buds 9 to15 per umbel, blah. "),
-    list(min=7,  max=9,  input="Inflorescence blah, buds 7–9 per umbel, blah. "),
-    list(min=3,  max=3,  input="Inflorescence blah, buds 3 (rarely 7) per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds 7 or sometimes irregular due to internode elongation within an umbel, blah. "),
-    list(min=11, max=15, input="Inflorescence blah, buds 11 to more than 15 per umbel, blah. "),
-    list(min=7,  max=19, input="Inflorescence blah, buds 7–19 per umbel, blah. "),
-    list(min=9,  max=27, input="Inflorescence blah, buds 9–27 per umbel, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds 7, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds per umbel 7, blah. "),
-    list(min=11, max=11, input="Inflorescence blah, buds 11 to many per umbel, blah. "),
-    list(min=11, max=19, input="Inflorescence blah, buds 11 to 19 per umbel, blah. "),
-    list(min=3,  max=7,  input="Inflorescence blah, buds 3 or 7 per umbel, blah. "),
-    list(min=1,  max=1,  input="Inflorescence blah, bud solitary, blah. "),
-    list(min=7,  max=7,  input="Inflorescence blah, buds usually 7 per umbel (rarely up to 11), pedicels 0.1–0.3 cm long. "),
-    list(min=11, max=15, input="Inflorescence blah, buds in umbels of 11 to 15(21), sessile or with pedicels to 0.3 cm long. "),
-    list(min=7,  max=15, input="Inflorescence blah, buds 7 to ?15, pedicellate (pedicels (0.3–0.6 cm long). "),
-    list(min=3,  max=3,  input="Inflorescence blah, buds 3, or 8 or 5, and something 6. ")
+    list(min = 7, max = 7, input = "Inflorescence blah, buds 7 per umbel, blah. "),
+    list(min = 7, max = 13, input = "Inflorescence blah, buds 7, 9, 11 or 13 per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, 7 buds per umbel, rarely 3, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds 7(–11) per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds per umbel 7, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds usually 7 (rarely 3) per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds more than 7 per umbel, blah. "),
+    list(min = 7, max = 9, input = "Inflorescence blah, buds 7 or more commonly 9 per umbel, blah. "),
+    list(min = 9, max = 9, input = "Inflorescence blah, buds 9 (rarely 11) per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds per umbel 7, blah. "),
+    list(min = 9, max = 15, input = "Inflorescence blah, buds 9 to15 per umbel, blah. "),
+    list(min = 7, max = 9, input = "Inflorescence blah, buds 7–9 per umbel, blah. "),
+    list(min = 3, max = 3, input = "Inflorescence blah, buds 3 (rarely 7) per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds 7 or sometimes irregular due to internode elongation within an umbel, blah. "),
+    list(min = 11, max = 15, input = "Inflorescence blah, buds 11 to more than 15 per umbel, blah. "),
+    list(min = 7, max = 19, input = "Inflorescence blah, buds 7–19 per umbel, blah. "),
+    list(min = 9, max = 27, input = "Inflorescence blah, buds 9–27 per umbel, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds 7, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds per umbel 7, blah. "),
+    list(min = 11, max = 11, input = "Inflorescence blah, buds 11 to many per umbel, blah. "),
+    list(min = 11, max = 19, input = "Inflorescence blah, buds 11 to 19 per umbel, blah. "),
+    list(min = 3, max = 7, input = "Inflorescence blah, buds 3 or 7 per umbel, blah. "),
+    list(min = 1, max = 1, input = "Inflorescence blah, bud solitary, blah. "),
+    list(min = 7, max = 7, input = "Inflorescence blah, buds usually 7 per umbel (rarely up to 11), pedicels 0.1–0.3 cm long. "),
+    list(min = 11, max = 15, input = "Inflorescence blah, buds in umbels of 11 to 15(21), sessile or with pedicels to 0.3 cm long. "),
+    list(min = 7, max = 15, input = "Inflorescence blah, buds 7 to ?15, pedicellate (pedicels (0.3–0.6 cm long). "),
+    list(min = 3, max = 3, input = "Inflorescence blah, buds 3, or 8 or 5, and something 6. ")
   )
 
   failed <- FALSE
@@ -106,8 +108,8 @@ test_extract_buds <- function() {
     result <- extract_buds(test$input)
     if (
       is.na(result$buds_input_type) ||
-      test$min != result$buds_min ||
-      test$max != result$buds_max
+        test$min != result$buds_min ||
+        test$max != result$buds_max
     ) {
       print(sprintf(
         "'%s': expected %d..%d, got %d..%d (%s)",
@@ -126,7 +128,6 @@ test_extract_buds <- function() {
   } else {
     cat("All tests OK\n")
   }
-
 }
 
 # -------
